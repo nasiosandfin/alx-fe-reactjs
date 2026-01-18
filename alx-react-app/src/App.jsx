cf1fd0df3d41da6aa08e7dd1afb31c99941f45c4
Sandfin Nasio <nasiosandfin@gmail.com>
2026-01-18 20:18:04 +0700

    Add Header, MainContent, and Footer components to App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,8 @@
+// App.jsx
+import React from 'react';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 import WelcomeMessage from './components/WelcomeMessage';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
