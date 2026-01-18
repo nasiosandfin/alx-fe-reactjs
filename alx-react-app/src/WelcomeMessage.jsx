34ead1c0aff2d5d9c0dff269d706ddbaae375827
Sandfin Nasio <nasiosandfin@gmail.com>
2026-01-18 18:50:23 +0700

    Add WelcomeMessage component and integrate into App.jsx

--- alx-react-app/src/WelcomeMessage.jsx
+++ alx-react-app/src/WelcomeMessage.jsx
@@ -1,6 +1,7 @@
 function WelcomeMessage() {
     return (
         <div>
+            <WelcomeMessage />
             <h1>Hello everyone, I am learning React at ALX!.</h1>
             <p>This is a simple JSX component.</p>
             <p>I am learning about JSX!</p>
