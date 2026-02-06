227418515ed9fffd80ca3b3340c69394e261b527
Sandfin Nasio <nasiosandfin@gmail.com>
2026-02-06 17:00:27 +0300

    Refactored App.jsx and added Counter component

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,5 +1,12 @@
-import { createContext } from 'react';
+import { useContext } from 'react';
+import UserContext from './UserContext';
 
-const UserContext = createContext();
-
-export default UserContext;
+function UserProfile() {
+  const userData = useContext(UserContext);
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
---
+++ alx-react-app-props/src/components/WelcomeMessage.jsx
@@ -0,0 +1,21 @@
+function WelcomeMessage() {
+<<<<<<< HEAD
+  return (
+    <div>
+      <h1>Hello everyone, I am learning React at ALX!</h1>
+      <p>This is a simple JSX component.</p>
+      <p>I am learning about JSX!</p>
+    </div>
+  );
+=======
+    return (
+        <div>
+            <h1>Hello everyone, I am learning React at ALX!</h1>
+            <p>This is a simple JSX component.</p>
+            <p>I am learning about JSX!</p>
+        </div>
+    );
+>>>>>>> 5f2c443d4558b153df0e22199b1b6e5281275037
+}
+
+export default WelcomeMessage;
