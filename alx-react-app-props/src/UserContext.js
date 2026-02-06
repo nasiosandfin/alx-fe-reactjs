27046eeee5edd977efd253bada84d5f092370215
Sandfin Nasio <nasiosandfin@gmail.com>
2026-02-06 17:47:08 +0300

    Resolved merge conflict in WelcomeMessage.jsx

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -9,15 +9,3 @@ export default UserContext;
 
 
 
-import { useContext } from 'react';
-import UserContext from './UserContext';
-
-function UserProfile() {
-  const userData = useContext(UserContext);
-  return (
-    <div>
-      <p>Name: {userData.name}</p>
-      <p>Email: {userData.email}</p>
-    </div>
-  );
-}
--- alx-react-app-props/src/components/WelcomeMessage.jsx
+++ alx-react-app-props/src/components/WelcomeMessage.jsx
@@ -1,5 +1,5 @@
+
 function WelcomeMessage() {
-<<<<<<< HEAD
   return (
     <div>
       <h1>Hello everyone, I am learning React at ALX!</h1>
@@ -7,15 +7,6 @@ function WelcomeMessage() {
       <p>I am learning about JSX!</p>
     </div>
   );
-=======
-    return (
-        <div>
-            <h1>Hello everyone, I am learning React at ALX!</h1>
-            <p>This is a simple JSX component.</p>
-            <p>I am learning about JSX!</p>
-        </div>
-    );
->>>>>>> 5f2c443d4558b153df0e22199b1b6e5281275037
 }
 
 export default WelcomeMessage;
