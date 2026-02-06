dd4f4da007f8eba4d201d6f3748a4d7ee800e29d
Sandfin Nasio <nasiosandfin@gmail.com>
2026-02-06 17:16:19 +0300

    Refactored App.jsx and added Counter component

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -9,3 +9,15 @@ export default UserContext;
 
 
 
+import { useContext } from 'react';
+import UserContext from './UserContext';
+
+function UserProfile() {
+  const userData = useContext(UserContext);
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
