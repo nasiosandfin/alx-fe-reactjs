506a81b5afb97ff402957209d821b20a6ece203d
Sandfin Nasio <nasiosandfin@gmail.com>
2026-02-06 07:41:43 +0300

    Added Counter component with useState and integrated into App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -40,4 +40,18 @@ function App() {
   )
 }
 
+
+
+
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div>
+      <h1>My React App</h1>
+      <Counter />
+    </div>
+  );
+}
+
 export default App;
