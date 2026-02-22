6697531803bb91cb2c29b15dd0eb249a1b0a36da
Sandfin Nasio <nasiosandfin@gmail.com>
2026-02-22 22:15:43 +0300

    Include public/index.html in Tailwind content paths

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-  theme: { extend: {} },
+  content: [
+    './index.html',
+    './public/index.html',
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 }
 
