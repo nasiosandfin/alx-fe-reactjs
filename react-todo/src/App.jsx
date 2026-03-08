d4dc88a605c3edf23e0d590156e74db05e120776
Sandfin Nasio <nasiosandfin@gmail.com>
2026-03-08 21:44:54 +0300

    Add data-testid attributes to TodoList for testing

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -47,4 +47,19 @@ function App() {
   );
 }
 
+
+
+
+import TodoList from "./components/TodoList";
+import TodoListTest from "./components/tests/TodoList.test";
+
+function App() {
+  return (
+    <div>
+      <TodoList />
+      <TodoListTest />
+    </div>
+  );
+}
+
 export default App;
